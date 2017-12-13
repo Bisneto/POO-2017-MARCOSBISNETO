653a1dac9c903452175f3a83e87905085b942720
Marcos Ferreira <marcosferreira100@hotmail.com>
2017-12-13 10:29:29 -0300

    AtividadeF

---
+++ AtividadeF/src/Model/ContaCorrente.java
@@ -0,0 +1,13 @@
+
+package Model;
+
+public class ContaCorrente extends Conta {
+    	public void Atualiza(double taxa) {
+		 this.SetSaldo(this.GetSaldo() + this.GetSaldo() * taxa * 2);
+	}
+	public void Depositar(double valor) {
+		this.Saldo += valor - 0.1;
+	}
+
+}
+
